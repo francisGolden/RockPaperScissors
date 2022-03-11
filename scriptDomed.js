e18cb88b85a909ef5ea49381781ee69cf6bb69d1
francisGolden <doria5@live.it>
2022-03-11 20:34:32 +0100

    scriptDomed.js index.html style.css

---
+++ scriptDomed.js
@@ -0,0 +1,98 @@
+let items = ["Rock", "Paper", "Scissors"];
+
+function computerPlay(){
+    
+    const randomElement = items[Math.floor(Math.random() * items.length)];
+    
+    return randomElement;
+}
+
+playerCount = 0;
+computerCount = 0;
+
+function playRound(playerSelection, computerSelection) {
+    
+    computerSelection = computerPlay();
+    let giocatore = document.querySelector("#player")
+    giocatore.innerHTML = "Player: " + playerSelection;
+    console.log("Player: " + playerSelection);
+
+    let computer = document.querySelector("#computer")
+    computer.innerHTML = "Computer: " + computerSelection;
+    console.log("Computer: " + computerSelection);
+
+    let risultato = document.querySelector("#risultato")
+    let playerScore = document.querySelector("#player-score")
+    let computerScore = document.querySelector("#computer-score")
+    sum = 0
+
+    if (playerSelection.toLowerCase() == items[0].toLowerCase() && computerSelection.toLowerCase() == items[1].toLowerCase()) {
+        risultato.innerHTML = "Computer wins"
+        console.log("Computer wins")
+        computerCount += 1;
+        computerScore.innerHTML = `${computerCount}`
+
+    } else if (playerSelection.toLowerCase() == items[1].toLowerCase() && computerSelection.toLowerCase() == items[0].toLowerCase()) {
+        risultato.innerHTML = "Player wins"
+        
+        playerCount += 1;
+        playerScore.innerHTML = `${playerCount}`
+
+    } else if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
+        risultato.innerHTML = "Nobody wins!"
+        
+
+    } else if (playerSelection.toLowerCase() == items[2].toLowerCase() && computerSelection.toLowerCase() == items[0].toLowerCase()) {
+        risultato.innerHTML = "Computer wins"
+        computerCount += 1;
+        computerScore.innerHTML = `${computerCount}`
+
+    } else if (playerSelection.toLowerCase() == items[0].toLowerCase() && computerSelection.toLowerCase() == items[2].toLowerCase()) {
+        risultato.innerHTML = "Player wins"
+
+        playerCount += 1;
+        playerScore.innerHTML = `${playerCount}`
+
+    } else if (playerSelection.toLowerCase() == items[1].toLowerCase() && computerSelection.toLowerCase() == items[2].toLowerCase()) {
+        risultato.innerHTML = "Computer wins"
+
+        computerCount += 1;
+        computerScore.innerHTML = `${computerCount}`
+
+    } else if (playerSelection.toLowerCase() == items[2].toLowerCase() && computerSelection.toLowerCase() == items[1].toLowerCase()) {
+        risultato.innerHTML = "Computer wins"
+
+        computerCount += 1;
+        computerScore.innerHTML = `${computerCount}`
+
+    }
+  
+
+}
+
+let seleziona = document.querySelectorAll('.selection')
+
+let winner = document.querySelector("#winner")
+
+Array.from(seleziona).forEach(function(bottone) {
+    bottone.addEventListener("click", function() {
+        playRound(bottone.textContent)
+        if (playerCount == 5) {
+            winner.innerHTML = "PLAYER WINS!!!!!! LETS GOOOO"
+             
+            
+        } else if (computerCount == 5) {
+            winner.innerHTML = "COMPUTER WINS!!!!!!!!"
+            
+        }
+    
+    })
+})
+
+
+
+
+
+
+
+
